092cca26a2429473bf536c7235374f551c727b74
Manuel Holtgrewe <manuel.holtgrewe@bih-charite.de>
2022-09-14 15:19:43 +0200

    feat: add bam-collect-doc command (#1)
    This allows collecting depth of coverage from a BAM file and store it
    in a BCF or VCF file.

---
+++ src/common/bcf.rs
@@ -0,0 +1,145 @@
+use rust_htslib::bcf;
+
+#[derive(Debug)]
+pub struct FormatInfo {
+    /// Guessed format.
+    pub format: bcf::Format,
+    /// Guessed compression status.
+    pub uncompressed: bool,
+}
+
+/// Return `FormatInfo` for the given filename.
+pub fn guess_bcf_format(filename: &str) -> FormatInfo {
+    if filename.ends_with(".bcf") {
+        FormatInfo {
+            format: bcf::Format::Bcf,
+            uncompressed: false,
+        }
+    } else if filename.ends_with(".vcf.gz") {
+        FormatInfo {
+            format: bcf::Format::Vcf,
+            uncompressed: false,
+        }
+    } else {
+        FormatInfo {
+            format: bcf::Format::Vcf,
+            uncompressed: true,
+        }
+    }
+}
+
+// /// Build new `bcf::Header`.
+// pub fn build_vcf_header(template: &bcf::header::HeaderView) -> Result<bcf::Header, anyhow::Error> {
+//     let mut header = bcf::Header::new();
+
+//     // Copy over sequence records.
+//     for record in template.header_records() {
+//         if let bcf::header::HeaderRecord::Contig { key: _, values } = record {
+//             header.push_record(
+//                 format!(
+//                     "##contig=<ID={},length={}>",
+//                     values
+//                         .get("ID")
+//                         .expect("source contig header does not have ID"),
+//                     values
+//                         .get("length")
+//                         .expect("source contig header does not have length")
+//                 )
+//                 .as_bytes(),
+//             );
+//         }
+//     }
+
+//     // Fields: ALT, INFO, FORMAT.
+//     let alts = vec![
+//         ("DEL", "Deletion"),
+//         ("DUP", "Duplication"),
+//         ("INV", "Inversion"),
+//         ("CNV", "Copy number variant"),
+//     ];
+//     for (id, desc) in alts {
+//         header.push_record(format!("##ALT=<ID={},length={}>", &id, &desc).as_bytes());
+//     }
+
+//     let infos = vec![
+//         ("AC", "A", "Integer", "Allele count in genotypes"),
+//         (
+//             "AN",
+//             "1",
+//             "Integer",
+//             "Total number of alleles in called genotypes",
+//         ),
+//         ("SVTYPE", "1", "String", "Type of structural variant"),
+//         ("CHR2", "1", "String", "Chromosome of end coordinate"),
+//         ("END", "1", "Integer", "End position of linear SV"),
+//         ("END2", "1", "Integer", "End position of BND"),
+//         ("STRANDS", "1", "String", "Breakpoint strandedness"),
+//         ("SVLEN", "1", "Integer", "SV length"),
+//         ("ALGORITHMS", ".", "String", "Source algorithms"),
+//     ];
+//     for (id, number, type_, desc) in infos {
+//         header.push_record(
+//             format!(
+//                 "##INFO=<ID={},Number={},Type={},Description={}>",
+//                 &id, &number, &type_, &desc
+//             )
+//             .as_bytes(),
+//         );
+//     }
+//     let formats = vec![
+//         ("GT", "1", "String", "Genotype"),
+//         ("cnmops", "1", "Integer", "Called by cnMOPS"),
+//         ("delly", "1", "Integer", "Called by Delly"),
+//         ("manta", "1", "Integer", "Called by Manta"),
+//         ("PR", "1", "Float", "Paired read evidence"),
+//         ("SR", "1", "Float", "Split read evidence"),
+//         ("RD", "1", "Float", "Read depth evidence"),
+//         ("VL", "1", "Integer", "SNV count left of CNV region"),
+//         ("VM", "1", "Integer", "SNV count within CNV region"),
+//         ("VR", "1", "Integer", "SNV count right of CNV region"),
+//         ("ROH", "1", "Integer", "Run of homozygosity"),
+//     ];
+//     for (id, number, type_, desc) in formats {
+//         header.push_record(
+//             format!(
+//                 "##FORMAT=<ID={},Number={},Type={},Description={}>",
+//                 &id, &number, &type_, &desc
+//             )
+//             .as_bytes(),
+//         );
+//     }
+
+//     // Add samples.
+//     for name in template.samples() {
+//         header.push_sample(name);
+//     }
+
+//     Ok(header)
+// }
+
+// pub fn collect_contigs(reader: &bcf::IndexedReader) -> Result<Vec<String>, anyhow::Error> {
+//     let mut result: Vec<String> = Vec::new();
+//     let header = reader.header();
+//     for rid in 0..header.contig_count() {
+//         result.push(std::str::from_utf8(header.rid2name(rid)?)?.to_string());
+//     }
+//     Ok(result)
+// }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use matches::assert_matches;
+
+    #[test]
+    fn test_guess_bcf_format() {
+        assert_matches!(guess_bcf_format("ex.vcf").format, bcf::Format::Vcf);
+        assert_eq!(guess_bcf_format("ex.vcf").uncompressed, true);
+        assert_matches!(guess_bcf_format("ex.vcf.gz").format, bcf::Format::Vcf);
+        assert_eq!(guess_bcf_format("ex.vcf.gz").uncompressed, false);
+        assert_matches!(guess_bcf_format("ex.bcf").format, bcf::Format::Bcf);
+        assert_eq!(guess_bcf_format("ex.bcf").uncompressed, false);
+        assert_matches!(guess_bcf_format("ex.xxx").format, bcf::Format::Vcf);
+        assert_eq!(guess_bcf_format("ex.xxx").uncompressed, true);
+    }
+}
---
+++ src/common/doc.rs
@@ -0,0 +1,72 @@
+use std::collections::{HashMap, HashSet};
+
+use rust_htslib::{bcf, bcf::Read};
+
+use crate::common::stats::Stats;
+
+/// Name of the autosomes.
+static NAMES_AUTOSOMES: &[&str] = &[
+    "1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15", "16", "17",
+    "18", "19", "20", "21", "22", "chr1", "chr2", "chr3", "chr4", "chr5", "chr6", "chr7", "chr8",
+    "chr9", "chr10", "chr11", "chr12", "chr13", "chr14", "chr15", "chr16", "chr17", "chr18",
+    "chr19", "chr20", "chr21", "chr22", // The following are for testing only.
+    "one", "two",
+];
+
+/// Name of the gonomosomes.
+static NAMES_ALLOSOMES: &[&str] = &["X", "Y", "chrX", "chrY"];
+
+/// Store information about read depth.
+pub struct MedianReadDepthInfo {
+    /// Median read depth by chromosome.
+    pub by_chrom: HashMap<String, f64>,
+    /// Median read depth on autosomes.
+    pub on_autosomes: f64,
+}
+
+/// Load DoC from file and compute median.
+pub fn load_doc_median(path: &str) -> Result<MedianReadDepthInfo, anyhow::Error> {
+    let autosomes: HashSet<String> = NAMES_AUTOSOMES.iter().map(|&s| s.to_string()).collect();
+    let all_chroms: HashSet<String> = (&[NAMES_ALLOSOMES, NAMES_AUTOSOMES])
+        .concat()
+        .iter()
+        .map(|s| s.to_string())
+        .collect();
+    let mut reader = bcf::Reader::from_path(path)?;
+    let mut rcvs_by_chrom: Vec<Vec<f64>> = vec![vec![]; reader.header().contig_count() as usize];
+
+    let mut record = reader.empty_record();
+    while let Some(result) = reader.read(&mut record) {
+        result?;
+        rcvs_by_chrom[record.rid().unwrap() as usize]
+            .push(record.format(b"RCV").float()?[0][0].into());
+    }
+
+    let mut by_chrom: HashMap<String, f64> = HashMap::new();
+    let mut rcvs_autosomes: Vec<f64> = Vec::new();
+    for rid in 0..(reader.header().contig_count()) {
+        let chrom = String::from_utf8(reader.header().rid2name(rid)?.to_vec())?;
+        if all_chroms.contains(&chrom) {
+            let median = if rcvs_by_chrom.is_empty() {
+                0.0
+            } else {
+                (&rcvs_by_chrom[rid as usize]).median()
+            };
+            by_chrom.insert(chrom.clone(), median);
+        }
+        if autosomes.contains(&chrom) {
+            rcvs_autosomes.append(&mut rcvs_by_chrom[rid as usize].to_vec());
+        }
+    }
+
+    let on_autosomes = if rcvs_autosomes.is_empty() {
+        0.0
+    } else {
+        (&rcvs_autosomes).median()
+    };
+
+    Ok(MedianReadDepthInfo {
+        by_chrom,
+        on_autosomes,
+    })
+}
